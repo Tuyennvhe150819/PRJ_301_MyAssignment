5c7f31335ea928189f19b49ef58a9c6934c06162
Nguyễn Văn Tuyên <tuyennvhe150819@fpt.edu.vn>
2022-07-07 14:57:05 +0700

    copy class & html & jsp from old project

---
+++ Attendance_PRJ301_Assignment/src/java/model/Student.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author TGDD
+ */
+public class Student {
+    private int id;
+    private String email;
+    private String password;
+    private String displayName;
+    private String code;
+    private String imageUrl ;
+
+    public Student() {
+    }
+
+    public Student(int id,String email, String password, String displayName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.displayName = displayName;
+    }
+    
+    public Student(int id, String email, String password, String displayName, String code, String imageUrl) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.displayName = displayName;
+        this.code = code;
+        this.imageUrl = imageUrl;
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+}
