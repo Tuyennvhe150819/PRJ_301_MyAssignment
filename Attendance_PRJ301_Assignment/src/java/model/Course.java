bf46f1a834218aa3d860ddf60c4c87eeb3836160
Nguyễn Văn Tuyên <tuyennvhe150819@fpt.edu.vn>
2022-07-07 21:46:26 +0700

    create class

--- Attendance_PRJ301_Assignment/src/java/model/Course.java
+++ Attendance_PRJ301_Assignment/src/java/model/Course.java
@@ -9,5 +9,48 @@ package model;
  * @author TGDD
  */
 public class Course {
+    private int id;
+    private String name;
+    private String code;
+
+    public Course() {
+    }
+
+    public Course(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
+    public Course(int id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
     
 }
